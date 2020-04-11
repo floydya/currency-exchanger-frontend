13d0ff3860ec92ab7cdf4ece1df914f496e3e6ec
Vlad Galatskiy <xfloydya@gmail.com>
2020-04-11 22:33:38 +0300

    Updates

---
+++ precache-manifest.e25dd973ebcda7ee8979228bcbfbdcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6512af2b0d2864fb28ba4c073bb1875b",
+    "url": "/currency-exchanger-frontend/index.html"
+  },
+  {
+    "revision": "67ae6b4fdac11f05176a",
+    "url": "/currency-exchanger-frontend/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "67ae6b4fdac11f05176a",
+    "url": "/currency-exchanger-frontend/static/js/2.2c19b9db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currency-exchanger-frontend/static/js/2.2c19b9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125d6ce71ca6af0f149f",
+    "url": "/currency-exchanger-frontend/static/js/main.4ff525f5.chunk.js"
+  },
+  {
+    "revision": "234cd0cfc1fb8e663f69",
+    "url": "/currency-exchanger-frontend/static/js/runtime-main.662acd07.js"
+  }
+]);
